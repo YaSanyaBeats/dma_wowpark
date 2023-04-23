1427a9d78cd6f477cdda6c1272aca0061b0217ba
YaSanyaBeats <alexandr191261@yandex.ru>
2023-04-23 15:00:18 +0700

    Create all sections

--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -2,6 +2,27 @@
 
 import "%modules%/header/header";
 
+/* Main */
+
+import "%modules%/stories/stories";
+import "%modules%/wow/wow";
+import "%modules%/lines/lines";
+import "%modules%/programs/programs";
+import "%modules%/promo/promo";
+import "%modules%/tabs/tabs";
+import "%modules%/birthday/birthday";
+import "%modules%/tickets/tickets";
+import "%modules%/combo/combo";
+import "%modules%/science/science";
+import "%modules%/video-reviews/video-reviews";
+import "%modules%/schedule/schedule";
+import "%modules%/excursion/excursion";
+import "%modules%/reviews/reviews";
+import "%modules%/partition/partition";
+import "%modules%/feedback/feedback";
+import "%modules%/contacts/contacts";
+
+
 /* Footer */
 
 import "%modules%/footer/footer";
