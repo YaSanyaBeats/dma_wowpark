93019447c8fe4a6c75881c6b7606ba9603d7ca86
YaSanyaBeats <alexandr191261@yandex.ru>
2023-04-24 23:55:11 +0700

    Adaptive reviews section

--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,7 +1,36 @@
 const reviewSwiper = new Swiper('.reviews__swiper', {
     spaceBetween: 25,
-    freeMode: true,
     direction: 'horizontal',
     slidesPerView: 'auto',
-    grabCursor: true
+    grabCursor: true,
+
+    freeMode: {
+        enabled: true,
+        sticky: true,
+    },
+    
+    centeredSlides: true,
+
+    breakpoints: {
+        768: {
+            centeredSlides: true,
+        },
+        1140: {
+            centeredSlides: false,
+            navigation: {
+                enable: false
+            },
+            pagination: {
+                enable: false
+            }
+        }
+    },
+    navigation: {
+        nextEl: '.reviews__swiper .swiper-button-next',
+        prevEl: '.reviews__swiper .swiper-button-prev',
+    },
+    pagination: {
+        el: '.reviews__swiper .swiper-pagination',
+        type: 'bullets',
+    },
 })
